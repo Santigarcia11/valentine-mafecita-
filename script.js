42c56ea8f09a29b3a40dc5a5a2955d6e0f3fd76e
Santigarcia11 <santi.garcia0110@gmail.com>
2025-09-24 00:30:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-onload = () => {
-    document.body.classList.remove("container");
-  };
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const letter = document.querySelector(".letter");
 
-  const wrapper = document.querySelector(".wrapper");
-  const openBtn = document.getElementById("openBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  
-  openBtn.addEventListener("click", () => {
-      wrapper.classList.add("open");
-      openBtn.style.display = "none";
-      closeBtn.style.display = "inline-block";
-  });
-  
-  closeBtn.addEventListener("click", () => {
-      wrapper.classList.remove("open");
-      closeBtn.style.display = "none";
-      openBtn.style.display = "inline-block";
-  });
-  
+openBtn.addEventListener("click", () => {
+  letter.style.display = "block";
+  openBtn.style.display = "none";
+  closeBtn.style.display = "inline-block";
+});
+
+closeBtn.addEventListener("click", () => {
+  letter.style.display = "none";
+  openBtn.style.display = "inline-block";
+  closeBtn.style.display = "none";
+});
